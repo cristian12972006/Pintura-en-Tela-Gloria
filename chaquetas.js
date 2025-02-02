9db5ebb915a08695fb8f9aa718947b1aa797c495
cristian12972006 <73295349+cristian12972006@users.noreply.github.com>
2025-02-01 20:11:30 -0500

    chaquetas.js

--- chaquetas.js
+++ chaquetas.js
@@ -16,19 +16,19 @@ function contactar(articulo) {
 }
 
 function mirarinstra(articulo){
-    let enlacePSE='';
-    if (articulo === 'colibris') {
-        enlacePSE= "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
-    } else if (articulo === 'animales') {
-        mensaje = "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
-    } else if (articulo === 'mandalas') {
-        mensaje = "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
-    } else if (mensaje === 'naturaleza') {
-        mensaje = "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
+        let enlacePSE='';
+        if (articulo === 'colibris') {
+            enlacePSE= "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
+        } else if (articulo === 'animales') {
+            enlacePSE = "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
+        } else if (articulo === 'mandalas') {
+            enlacePSE = "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
+        } else if (mensaje === 'naturaleza') {
+            enlacePSE= "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
+        }
+        
+
+        window.open(enlacePSE,'_blank');
+
+
     }
-    
-
-    window.open(enlacePSE,'_blank');
-
-
-}
