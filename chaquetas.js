cd938ad2941d7ce1cd2f21976cddd6d491b0a2c4
cristian12972006 <73295349+cristian12972006@users.noreply.github.com>
2025-02-01 19:57:26 -0500

    PinturasGloria

---
+++ chaquetas.js
@@ -0,0 +1,34 @@
+function contactar(articulo) {
+    let mensaje = '';
+    if (articulo === 'colibris') {
+        mensaje = "¡Hola! Estoy interesado en la chaqueta pintada a mano con colibríes.";
+    } else if (articulo === 'animales') {
+        mensaje = "¡Hola! Estoy interesado en la chaqueta pintada a mano con animales.";
+    } else if (articulo === 'mandalas') {
+        mensaje = "¡Hola! Estoy interesado en la chaqueta pintada a mano con mandalas.";
+    } else if (mensaje === 'naturaleza') {
+        mensaje = "¡Hola! Estoy interesado en la chaqueta pintada a mano con naturaleza.";
+    }
+
+    let numeroWhatsapp= '573137074505';
+    let urlWhatsapp=`https://wa.me/${numeroWhatsapp}?text=${encodeURIComponent(mensaje)}`;
+    window.open(urlWhatsapp,'_blank');
+}
+
+function mirarinstra(articulo){
+    let enlacePSE='';
+    if (articulo === 'colibris') {
+        enlacePSE= "https://www.instagram.com/gloriacruz_pt?igsh=Nnh6a3dmZXQ2MmRn";
+    } else if (articulo === 'animales') {
+        mensaje = "¡Hola! Estoy interesado en la chaqueta pintada a mano con animales.";
+    } else if (articulo === 'mandalas') {
+        mensaje = "¡Hola! Estoy interesado en la chaqueta pintada a mano con mandalas.";
+    } else if (mensaje === 'naturaleza') {
+        mensaje = "¡Hola! Estoy interesado en la chaqueta pintada a mano con naturaleza.";
+    }
+    
+
+    window.open(enlacePSE,'_blank');
+
+
+}
